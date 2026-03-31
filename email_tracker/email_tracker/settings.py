1c73caad398c684db5736556b360e9796a94de9a
abhayrathor12 <rathorabhay633@gmail.com>
2026-03-31 17:11:38 +0530

    www

--- email_tracker/email_tracker/settings.py
+++ email_tracker/email_tracker/settings.py
@@ -126,7 +126,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-# EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_HOST_USER = 'workspace00018@gmail.com'
-EMAIL_HOST_PASSWORD = 'gobd owgw ihks kqxe'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
